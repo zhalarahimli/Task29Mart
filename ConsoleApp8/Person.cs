6af28fc69eebb7a3619e692540dafafaac3ef89a
zhalarahimli00@gmail.com <zhalarahimli00@gmail.com>
2024-03-28 12:30:09 +0400

    task finished

---
+++ ConsoleApp8/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Person
+    {
+        public string Name;
+        public string Surname;
+        public int Age;
+        public bool IsAdult;
+
+
+        public Person(string name, string surname, int age) 
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+            if (Age >= 18)
+            {
+                this.IsAdult = true;
+
+            }
+            else { IsAdult = false; }
+        }
+    }
+
+}
